b9d3b3ec7fc242407cc4dba9993f7d5e7d35a0f0
USE104 <jose.hssilva4@senacsp.edu.br>
2022-08-29 11:19:01 -0300

    actions div

---
+++ script.js
@@ -0,0 +1,13 @@
+//const paragrafo = document.createElement('p')
+
+//paragrafo.innerText = "Isso é um paragrafo"
+
+//document.body.appendChild(paragrafo)
+
+function minhaFuncao(){
+    document.getElementById("minhaDiv").classList.add("classe1","classe2","classe3")
+}
+
+function remover(){
+    document.getElementById("minhaDiv").classList.remove("classe1","classe2","classe3")
+}
